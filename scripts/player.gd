3a517762f6e5280c0f565d2121b316314e2a551b
brent-000 <billingsleybrent47@gmail.com>
2024-11-26 17:37:14 -0600

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+const COYOTE_TIME = 0.2
+
+var coyote_timer = 0.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	update_coyote_timer(delta)
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else:
+		coyote_timer = COYOTE_TIME
+
+	# Handle jump.
+	if coyote_timer > 0:
+		if Input.is_action_just_pressed("ui_accept"):
+			coyote_timer = 0
+			velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+func update_coyote_timer(delta):
+	if not is_on_floor() and coyote_timer > 0:
+		coyote_timer -= delta
