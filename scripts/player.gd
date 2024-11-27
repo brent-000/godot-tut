064ed8a586b2f710413b4f6eb6c80adbeb55313a
brent-000 <billingsleybrent47@gmail.com>
2024-11-27 17:56:07 -0600

    flipping h

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,8 @@ const COYOTE_TIME = 0.2
 
 var coyote_timer = 0.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	update_coyote_timer(delta)
@@ -25,8 +27,12 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction == 1:
+		animated_sprite_2d.flip_h = false
 		velocity.x = direction * SPEED 
+	elif direction == -1:
+		animated_sprite_2d.flip_h = true
+		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
