f3455b208df282b848e11020bcf5b810d7194bf9
brent-000 <billingsleybrent47@gmail.com>
2024-11-27 18:06:19 -0600

    dying and keybinds

--- scripts/player.gd
+++ scripts/player.gd
@@ -20,13 +20,13 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if coyote_timer > 0:
-		if Input.is_action_just_pressed("ui_accept"):
+		if Input.is_action_just_pressed("jump"):
 			coyote_timer = 0
 			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction == 1:
 		animated_sprite_2d.flip_h = false
 		velocity.x = direction * SPEED 
